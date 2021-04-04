9d973e849cbfbec34fcaf99816ca0cef55216a28
Alayna Buysse <alaynagrace@comcast.net>
2021-04-03 20:12:26 -0500

    try next

--- traps.h
+++ traps.h
@@ -26,9 +26,7 @@
 // processor defined exceptions or interrupt vectors.
 #define T_SYSCALL       64      // system call
 #define T_DEFAULT      500      // catchall
-
 #define T_IRQ0          32      // IRQ 0 corresponds to int T_IRQ
-
 #define IRQ_TIMER        0
 #define IRQ_KBD          1
 #define IRQ_COM1         4
