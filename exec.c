7dfb756ff4aa3f77e00a55aa674abe9d0c9fad49
Alayna Buysse <alaynagrace@comcast.net>
2021-04-03 20:22:52 -0500

    try

--- exec.c
+++ exec.c
@@ -39,7 +39,12 @@ exec(char *path, char **argv)
     goto bad;
 
   // Load program into memory.
-  sz = 0;
+  // Allocate a page of memory for NULL
+	sz = 0;
+	if((sz = allocuvm(pgdir, sz, sz + PGSIZE)) == 0)
+ 		goto bad;
+	clearpteu(pgdir, (char*)(sz - PGSIZE));
+  
   for(i=0, off=elf.phoff; i<elf.phnum; i++, off+=sizeof(ph)){
     if(readi(ip, (char*)&ph, off, sizeof(ph)) != sizeof(ph))
       goto bad;
