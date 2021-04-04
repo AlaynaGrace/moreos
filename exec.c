b9eb4f2c17f6a62dc49ec2ad7c324a214856c2a4
Alayna Buysse <alaynagrace@comcast.net>
2021-04-04 18:03:21 -0500

    readd

--- exec.c
+++ exec.c
@@ -77,7 +77,7 @@ exec(char *path, char **argv)
   if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
     goto bad;
   // clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
-  // sp = sz;
+  sp = sz;
 
   // Push argument strings, prepare rest of stack in ustack.
   for(argc = 0; argv[argc]; argc++) {
