39dfb15d6d55d46c1b9b112c7cbd7c48b4aad763
Alayna Buysse <alaynagrace@comcast.net>
2021-04-04 17:50:55 -0500

    try

--- trap.c
+++ trap.c
@@ -78,15 +78,15 @@ trap(struct trapframe *tf)
     lapiceoi();
     break;
   case T_PGFLT:
-			if(growstack(myproc()->pgdir, myproc()->tf->esp, myproc()->topStack, myproc()) == 0)
-				break;
-			// cprintf("pid %d %s: page fault on %d eip 0x%x ",myproc()->pid, myproc()->name, cpuid()->apicid, tf->eip);
-			cprintf("stack 0x%x sz 0x%x addr 0x%x\n", myproc()->topStack, myproc()->sz, rcr2());
-			if(myproc()->tf->esp > myproc()->sz)
-				deallocuvm(myproc()->pgdir, USERTOP, myproc()->topStack);
-
-			myproc()->killed = 1;
+		if(growstack(myproc()->pgdir, myproc()->tf->esp, myproc()->topStack, myproc()) == 0)
 			break;
+		cprintf("pid %d %s: page fault on %d eip 0x%x ",myproc()->pid, myproc()->name, cpuid()->apicid, tf->eip);
+		cprintf("stack 0x%x sz 0x%x addr 0x%x\n", myproc()->topStack, myproc()->sz, rcr2());
+		if(myproc()->tf->esp > myproc()->sz)
+			deallocuvm(myproc()->pgdir, USERTOP, myproc()->topStack);
+
+		myproc()->killed = 1;
+		break;
 
   //PAGEBREAK: 13
   default:
